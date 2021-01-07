18dfab98bbd86325e11ffde18d43383e3e05c55c
Tou Xiong <touyeexiong@gmail.com>
2021-01-06 21:25:50 -0600

    added server get to retrieve menu from api

--- server/routes/getMenu.router.js
+++ server/routes/getMenu.router.js
@@ -1,10 +1,16 @@
+const { default: Axios } = require('axios');
+const { response } = require('express');
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
 router.get('/', (req,res) => {
     console.log(`we in getMenu`);
-    
+    axios.get(`https://sandbox.dev.clover.com/v3/merchants/${process.env.SMOKIN_MERCH_ID}?${SMOKIN_API_TOKEN}`)
+    .then((response) => {
+        console.log(response.data);
+        res.send(response.data)
+    })
 })
 
 module.exports = router;
