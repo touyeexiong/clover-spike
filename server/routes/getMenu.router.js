cdab460d23f9bf5bbcf45355d3619a2f20b18d8b
Tou Xiong <touyeexiong@gmail.com>
2021-02-12 16:18:32 -0600

    removed console log on server.

--- server/routes/getMenu.router.js
+++ server/routes/getMenu.router.js
@@ -7,7 +7,6 @@ router.get('/', (req,res) => {
     console.log(`we in getMenu`);
     axios.get(`https://sandbox.dev.clover.com/v3/merchants/${process.env.SMOKIN_MERCH_ID}/items?access_token=${process.env.SMOKIN_API_TOKEN}`)
     .then((response) => {
-        console.log(response.data);
         res.send(response.data.elements)
     })
     .catch((err) => {
